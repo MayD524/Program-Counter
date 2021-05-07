0f3e4b2fed45ece64b1f911bb8537303fc953016
Ryan <57610348+RyanD524@users.noreply.github.com>
2021-05-07 19:34:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ def main(ending):
         if os.path.isdir(i):
             continue
         if ending == "all" or i.endswith(ending):
-            data = len(UPL.Core.file_manager.read_file(f"{os.getcwd()}/{i}"))
-            print(f"file '{i}' is {data} lines")
-            total += data
-    print(f"Total Lines: {total} lines")
+            data = UPL.Core.file_manager.clean_read(f"{os.getcwd()}/{i}")
+            print(f"file '{i}' is {len(data)} lines")
+            total += len(data)
+            
+    print(f"Total Lines: {total} lines (without whitespaces)")
             
 if __name__ == "__main__":
     ending = "all"
